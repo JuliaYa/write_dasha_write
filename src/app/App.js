5c56caf0d09ee4099dbae7869290416989cab2cc
Julia Yakutova <julia.yakutova@gmail.com>
2021-02-25 16:53:04 +0300

    Clean up project.

---
+++ src/app/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Write Dasha Write!</h1>
+      </header>
+      <div className="content">
+        <p>Here will be the demo editor soon. <br />
+          Just wait a little bit :)</p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
